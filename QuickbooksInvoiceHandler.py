bfd0462ce06f55823d4297107f4769917dfc8b42
JM <johnmarseglia32@gmail.com>
2026-04-16 18:02:56 -0400

    fixed csv column check

--- QuickbooksInvoiceHandler.py
+++ QuickbooksInvoiceHandler.py
@@ -179,9 +179,7 @@ class QuickbooksInvoiceHandler():
         reader = csv.DictReader(file)
         if reader is None or reader.fieldnames is None:
             raise Exception(f"CSV file {filename} is empty.")
-        print(self.config.csv_config.values())
         for col in reader.fieldnames:
-            print(col)
-            if col not in self.config.csv_config.keys():
+            if col not in self.config.csv_config.values():
                 raise Exception(f"Column name {col} does not exist in the config.")
         return reader
